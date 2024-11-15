bb9eb578189421a31e7c98670e07b45445877217
Muaz <6muazx@gmail.com>
2024-11-15 18:24:23 +0530

    Added Feature

--- Python/Learning/arduino.py
+++ Python/Learning/arduino.py
@@ -1,26 +1,28 @@
-import pyfirmata
-from pyfirmata import Arduino, util
-import time
+# import pyfirmata
+# from pyfirmata import Arduino, util
+# import time
 
-board = pyfirmata.Arduino('COM3')
-print("Communication Started!")
+# board = pyfirmata.Arduino('COM3')
+# print("Communication Started!")
 
-iterator = util.Iterator(board)
-iterator.start()
+# iterator = util.Iterator(board)
+# iterator.start()
 
-LED = board.digital[9]
-LED.mode = pyfirmata.PWM
+# LED = board.digital[9]
+# LED.mode = pyfirmata.PWM
 
-pwm_counter = 0.01
-increase_pwm = True
-while True:
-    if increase_pwm:
-        pwm_counter += 0.01
-        if pwm_counter >= 1:
-            increase_pwm = False
-    else:
-        pwm_counter -= 0.01
-        if pwm_counter <= 0:
-            increase_pwm = True
-    LED.write(pwm_counter)
-    time.sleep(0.01)
+# pwm_counter = 0.01
+# increase_pwm = True
+# while True:
+#     if increase_pwm:
+#         pwm_counter += 0.01
+#         if pwm_counter >= 1:
+#             increase_pwm = False
+#     else:
+#         pwm_counter -= 0.01
+#         if pwm_counter <= 0:
+#             increase_pwm = True
+#     LED.write(pwm_counter)
+#     time.sleep(0.01)
+
+print("I am testing a new repo feature")
